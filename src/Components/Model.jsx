26ba700e3f82192c41ebedb9897233ba00f0f3a4
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-07 01:02:52 +0530

    first 3d spline model is implemented

---
+++ src/Components/Model.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function Model() {
+  return (
+    <div className='w-auto h-[500px  flex justify-center'>
+        <Spline className='' scene="https://prod.spline.design/0uAVC2MdlnQ9-O-F/scene.splinecode" />
+    </div>
+  );
+}
+
